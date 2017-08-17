55c5b872934d82a3b4b2ce2ab472163171444cc2
John Scherff <jscherff@gmail.com>
2017-08-16 22:00:58 -0700

    Removed duplicate GetFunctionInfo function.

--- device.go
+++ device.go
@@ -227,21 +227,6 @@ func (d *Device) VendorReset() (int, error) {
 // ============================================================================
 
 /*
- * Get function filename, line number, and function name for error reporting.
- */
-func getFunctionInfo() string {
-
-	pc, file, line, success := runtime.Caller(1)
-	function := runtime.FuncForPC(pc)
-
-	if !success {
-		return "Unknown goroutine"
-	}
-
-	return fmt.Sprintf("%s:%d: %s()", filepath.Base(file), line, function.Name())
-}
-
-/*
  * Get the device descriptor of the device.
  */
 func (d *Device) getDeviceDescriptor() (error) {
